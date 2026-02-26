bfd24e56554a669918ab5a3ec4336b2fcb4793b6
azizben01 <benazizsang1@gmail.com>
2026-02-26 17:03:05 +0200

    first commit training website

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Particles from "./Particles";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -49,6 +50,7 @@ export default function RootLayout({
         </div> */}
         <div className="relative z-10">
         {children}
+        <Analytics /> {/* Add this line */}
 
         </div>
          
